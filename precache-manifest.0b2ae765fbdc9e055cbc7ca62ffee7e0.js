46434c376c79abb3501775b0f5c2aea2599f009f
Tzikas <niko.tzikas@gmail.com>
2019-11-11 19:54:35 -0500

    Updates

---
+++ precache-manifest.0b2ae765fbdc9e055cbc7ca62ffee7e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5138040a4c6344d8903f374831062079",
+    "url": "/characters/index.html"
+  },
+  {
+    "revision": "ee3985abd9f18ebd33b3",
+    "url": "/characters/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8a00e8d5180a98510642",
+    "url": "/characters/static/js/2.050b0125.chunk.js"
+  },
+  {
+    "revision": "ee3985abd9f18ebd33b3",
+    "url": "/characters/static/js/main.057adf8c.chunk.js"
+  },
+  {
+    "revision": "68fa6e6293c9fcbed0d1",
+    "url": "/characters/static/js/runtime-main.96994d40.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/characters/static/media/logo.25bf045c.svg"
+  }
+]);
